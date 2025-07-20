6247b366557ea2f0501f4feeaf24763610f36126
Pablo <p.grabchikov@gmail.com>
2025-07-20 23:19:09 +0300

    google form visual infrovements

--- src/components/Pages/Feedback.jsx
+++ src/components/Pages/Feedback.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 function Feedback() {
   return (
     <div>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSehOg-8xhMCpCtmEkToO_Jjvy2qLv4pfkIzghOBECpelkE-3w/viewform?embedded=true" width="900" height="1924" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSehOg-8xhMCpCtmEkToO_Jjvy2qLv4pfkIzghOBECpelkE-3w/viewform?embedded=true" width="100%" height="2020" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   );
 }
