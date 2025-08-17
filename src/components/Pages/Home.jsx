7ee1df2060598336aaee6fadf100a3ffc9781f49
rytismak <rytis.makauskas@gmail.com>
2025-08-17 23:01:20 +0300

    Update Home.jsx
    Shorter bullet points at the end of the page and added a disclaimer.

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -49,15 +49,13 @@ function Home() {
             <strong>
               Contextualize financial data
             </strong>{" "}
-            by comparing companies against their industry peers, giving you a
-            clearer picture of their performance.
+            by comparing companies against their industry peers.
           </li>
           <li>
             <strong>
               Simplify complex valuation
             </strong>{" "}
-            so you can understand what makes a good business, without getting
-            lost in the details.
+            so you can understand what makes a good business.
           </li>
         </ul>
         <p>
@@ -70,6 +68,28 @@ function Home() {
             This is a preview version for testing purposes. We'd love your
             feedback as we refine Oakie to be the best tool for aspiring
             investors!
+          </em>
+        </p>
+        <p className="text-muted">
+          <em>
+            The content and tools provided on this website are for informational and educational purposes only. 
+            Nothing on this website should be interpreted as financial advice, investment recommendations, 
+            or an endorsement to buy or sell any security.
+          </em>
+        </p>
+        <p className="text-muted">
+          <em>
+            All data, analyses, and metrics, including intrinsic value and other valuation models, 
+            are estimations and should not be considered a guarantee of a company's future performance 
+            or its true market worth. The information is provided "as is" and without warranties of any kind, 
+            either express or implied.
+          </em>
+        </p>
+        <p className="text-muted">
+          <em>
+            All investment decisions carry a risk of loss. 
+            Past performance is not indicative of future results. 
+            You should perform your own due diligence and research before making any investment decisions.
           </em>
         </p>
       </section>
