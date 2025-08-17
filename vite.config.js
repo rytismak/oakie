49f95113d80ce2987d754785eb999cee8568da05
rytismak <rytis.makauskas@gmail.com>
2025-08-17 10:12:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/oakie-dev/",
+  base: "/oakie/",
   plugins: [react()],
   test: {
     environment: "jsdom",
